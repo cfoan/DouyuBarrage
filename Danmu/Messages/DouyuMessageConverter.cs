440c5c2b8ff59166be1080153bdb637909824053
Zhong Jerry <Zhong Jerry>
2017-04-11 23:03:47 +0800

    initial commit

---
+++ Danmu/Messages/DouyuMessageConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DouyuDanmu.Messages
+{
+    public abstract class DouyuMessageConverter<TDouyuMessage>
+    {
+        public abstract TDouyuMessage ParseString(string douyuMessage);
+
+        public abstract string DumpsString(TDouyuMessage douyuMessage);
+    }
+}
