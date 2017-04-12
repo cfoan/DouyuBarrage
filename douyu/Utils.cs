5fae043f51572460a055ebad73fad805efd694f3
JerryZhongCN <1029554868@qq.com>
2017-04-12 22:07:35 +0800

    添加获取roomId方法

--- douyu/Utils.cs
+++ douyu/Utils.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Douyu
 {
     public class Utils
     {
+        private const string RegexRoomId = "\"room_id\":(\\d*),";
         /// <summary>
         /// 获取unix时间
         /// </summary>
@@ -34,6 +36,22 @@ namespace Douyu
                 if (count > 0 && index1 + 1 + count <= response.Length)
                 {
                     return response.Substring(index1 + 1, index2 - index1 - 1);
+                }
+            }
+            return "";
+        }
+
+        public static string GetRoomId2(Uri uri)
+        {
+            WebRequest request = WebRequest.Create(uri);
+            using (var sr = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                var response = sr.ReadToEnd();
+                Regex regex = new Regex(RegexRoomId, RegexOptions.IgnoreCase);
+                var match=regex.Match(response);
+                if (match.Success&& match.Groups.Count>1)
+                {
+                    return match.Groups[1].Value;
                 }
             }
             return "";
